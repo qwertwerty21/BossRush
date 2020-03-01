72930558a3edaada307a95275b57814d54663ca9
sunny <sunny@sunnys-MacBook-Pro.local>
2020-02-29 23:07:21 -0800

    initial commit

---
+++ Assets/Player/ThirdPersonController/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 10f;
+    [SerializeField] Transform defaultTarget;
+    [SerializeField] Transform player;
+
+    private Transform currentTarget;
+    private float mouseX;
+    private float mouseY;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        currentTarget = defaultTarget;
+    }
+
+    private void LateUpdate()
+    {
+        CamControl();
+    }
+
+    private void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35f, 60f);
+
+      
+        transform.LookAt(currentTarget);
+        Debug.Log("currentTarget " + currentTarget.name);
+
+       
+        currentTarget.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
+        player.rotation = Quaternion.Euler(0f, mouseX, 0f);
+    }
+
+}
---
+++ Assets/Player/ThirdPersonController/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 20f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0, ver).normalized * moveSpeed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
