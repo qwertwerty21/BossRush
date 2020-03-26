7b48540801f1398b2e5455c49f479a350121590d
sunny <sunny@sunnys-MacBook-Pro.local>
2020-03-25 19:52:33 -0700

    adds crosshair and hit effects

---
+++ .history/Assets/Player/Scripts/ProjectileWeapon_20200324181714.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileWeapon : MonoBehaviour
+{
+
+  [SerializeField] Camera m_Camera;
+  [SerializeField] float m_Range = 100f;
+  [SerializeField] float m_Damage = 30f;
+  [SerializeField] float m_TimeBetweenShots = 0.5f;
+  [SerializeField] ParticleSystem m_MuzzleFlash;
+  [SerializeField] GameObject m_HitEffect;
+  [SerializeField] string m_CrosshairName;
+  private bool m_CanShoot = true;
+  private Sprite m_CrosshairSprite;
+  private Canvas m_Canvas;
+  // Layer 8 is the Player Layer
+  // Bit shift the index of the layer (8) to get a bit mask
+  // This would cast rays only against colliders in layer 8.
+  // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+  private int m_LayerMask = ~(1 << 8);
+
+  private void SetCrosshair()
+  {
+    m_CrosshairSprite = Resources.Load<Sprite>(m_CrosshairName);
+    m_Canvas = GetComponentInParent<Canvas>();
+    m_Canvas.GetComponent<Crosshair>().image.sprite = m_CrosshairSprite;
+  }
+
+  IEnumerator Shoot()
+  {
+    m_CanShoot = false;
+    PlayMuzzleFlash();
+    ProcessRaycast();
+    yield return new WaitForSeconds(m_TimeBetweenShots);
+    m_CanShoot = true;
+  }
+
+  private void PlayMuzzleFlash()
+  {
+    m_MuzzleFlash.Play();
+  }
+
+  private void ProcessRaycast()
+  {
+    RaycastHit hit;
+
+
+    if (Physics.Raycast(m_Camera.transform.position, m_Camera.transform.forward, out hit, m_Range, m_LayerMask))
+    {
+      Debug.Log("hit collider gameobject " + hit.collider.gameObject);
+      if (hit.transform.tag == "Enemy")
+      {
+
+        Debug.Log("HIT " + hit);
+        CreateHitImpact(hit);
+      }
+    }
+    else
+    {
+      return;
+    }
+  }
+
+  private void CreateHitImpact(RaycastHit hit)
+  {
+    GameObject impact = Instantiate(m_HitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+    Destroy(impact, 1f);
+  }
+
+  private void Awake()
+  {
+    SetCrosshair();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0) && m_CanShoot)
+    {
+      StartCoroutine(Shoot());
+    }
+  }
+}
